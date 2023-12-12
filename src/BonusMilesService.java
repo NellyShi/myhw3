75595766dbecb349da3eb08325625b51131e65ec
Nelly Shigapova <nelly-shig@yandex.ru>
2023-12-12 21:25:22 +0400

    Make task

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,6 @@
 public class BonusMilesService {
     public int calculate(int ticketPrice){
         int oneMileCost = 20;
-//        int ticketPrice = 16589;
         return ticketPrice / oneMileCost;
     }
-
 }
